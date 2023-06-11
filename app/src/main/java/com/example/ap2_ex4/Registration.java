474125054ab33236b1f36daa459bfa5e330e2871
spitzerhadar <116832506+spitzerhadar@users.noreply.github.com>
2023-06-11 17:15:00 +0300

    register

---
+++ app/src/main/java/com/example/ap2_ex4/Registration.java
@@ -0,0 +1,38 @@
+package com.example.ap2_ex4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Registration extends AppCompatActivity {
+    private static final int SELECT_IMAGE = 1;  // This is the line that defines the constant
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button selectImageButton = findViewById(R.id.selectImageButton);
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent selectImageIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(selectImageIntent, SELECT_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            // Do something with the selected image Uri here
+        }
+    }
+}
