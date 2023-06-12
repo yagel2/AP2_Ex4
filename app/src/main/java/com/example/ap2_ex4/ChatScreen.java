5644d4db10f8f7385d74d33caabf01e32abcd45e
yagel2 <yagel22@gmail.com>
2023-06-12 12:06:01 +0300

    chat screen name change

--- app/src/main/java/com/example/ap2_ex4/ChatScreen.java
+++ app/src/main/java/com/example/ap2_ex4/ChatScreen.java
@@ -8,6 +8,6 @@ public class ChatScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_screen);
+        setContentView(R.layout.chat_screen);
     }
 }
