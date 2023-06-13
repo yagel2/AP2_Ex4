e1b2fe1a841a0eb5958da0d6efac6c6c953c7660
spitzerhadar <116832506+spitzerhadar@users.noreply.github.com>
2023-06-13 10:40:27 +0300

    validation register+connect

--- app/src/main/java/com/example/ap2_ex4/Registration.java
+++ app/src/main/java/com/example/ap2_ex4/Registration.java
@@ -5,16 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Registration extends AppCompatActivity {
-    private static final int SELECT_IMAGE = 1;  // This is the line that defines the constant
+    private static final int SELECT_IMAGE = 1;
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private EditText confirmPasswordInput;
+    private EditText displayNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
+
+        usernameInput = findViewById(R.id.editTextTextUsername);
+        passwordInput = findViewById(R.id.editTextTextPassword);
+        confirmPasswordInput = findViewById(R.id.editTextTextConfirmPassword);
+        displayNameInput = findViewById(R.id.editTextDisplayName);
 
         Button selectImageButton = findViewById(R.id.selectImageButton);
         selectImageButton.setOnClickListener(new View.OnClickListener() {
@@ -25,14 +40,51 @@ public class Registration extends AppCompatActivity {
             }
         });
 
-        Button connectButton = (Button) findViewById(R.id.register);
+        Button connectButton = findViewById(R.id.register);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Registration.this, Connection.class);
-                startActivity(intent);
+                if (validateInputs()) {
+                    Intent intent = new Intent(Registration.this, Connection.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    private boolean validateInputs() {
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String confirmPassword = confirmPasswordInput.getText().toString();
+        String displayName = displayNameInput.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (password.length() < 8 || !isAlphanumeric(password)) {
+            Toast.makeText(this, "Password must have at least 8 characters and contain both letters and numbers", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Password and confirm password do not match", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (TextUtils.isEmpty(displayName)) {
+            Toast.makeText(this, "Display name cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isAlphanumeric(String str) {
+        Pattern p = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+        Matcher m = p.matcher(str);
+        return m.matches();
     }
 
     @Override
