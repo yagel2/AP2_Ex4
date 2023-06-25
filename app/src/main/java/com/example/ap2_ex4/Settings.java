00c9407a0c5d2ec93b84b2513d641ccc27e63d20
spitzerhadar <116832506+spitzerhadar@users.noreply.github.com>
2023-06-25 21:39:57 +0300

    themes

--- app/src/main/java/com/example/ap2_ex4/Settings.java
+++ app/src/main/java/com/example/ap2_ex4/Settings.java
@@ -1,24 +1,114 @@
+//package com.example.ap2_ex4;
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.ImageButton;
+//import android.widget.Switch;
+//import androidx.appcompat.app.AlertDialog;
+//import android.annotation.SuppressLint;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.example.ap2_ex4.contacts.Contacts;
+//
+//public class Settings extends AppCompatActivity {
+//
+//    private int themeIndex = 0;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setTheme(getThemeId());
+//        LocaleHelper.setLocale(this, LocaleHelper.getSelectedLanguage(this));
+//        setContentView(R.layout.settings);
+//
+//        @SuppressLint("UseSwitchCompatOrMaterialCode")
+//        Switch switchLanguage = findViewById(R.id.switchLanguage);
+//        switchLanguage.setChecked(LocaleHelper.getSelectedLanguage(this).equals("he"));
+//        switchLanguage.setText(LocaleHelper.getSelectedLanguage(this).equals("he") ? R.string.Hebrew : R.string.English);
+//        switchLanguage.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//            if (isChecked) {
+//                LocaleHelper.setLocale(this, "he");
+//            } else {
+//                LocaleHelper.setLocale(this, "en");
+//            }
+//            updateLanguage();
+//        });
+//
+//        @SuppressLint("UseSwitchCompatOrMaterialCode")
+//        Switch switchDayNightMode = findViewById(R.id.switchDayNightMode);
+//        switchDayNightMode.setText(R.string.dayMode);
+//        switchDayNightMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//            if (isChecked) {
+//                switchDayNightMode.setText(R.string.nightMode);
+//            } else {
+//                switchDayNightMode.setText(R.string.dayMode);
+//            }
+//        });
+//
+//        Button buttonChangeTheme = findViewById(R.id.buttonThemeColor);
+//        buttonChangeTheme.setOnClickListener(v -> {
+//            new AlertDialog.Builder(this)
+//                    .setTitle("Choose Theme")
+//                    .setItems(new String[]{"Theme 1", "Theme 2", "Theme 3"}, (dialog, which) -> {
+//                        SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+//                        sharedPreferences.edit().putInt("themeIndex", which).apply();
+//                        recreate();
+//                    })
+//                    .create().show();
+//        });
+//
+//        Button buttonLogout = findViewById(R.id.buttonLogout);
+//        buttonLogout.setOnClickListener(v -> {
+//            Intent intent = new Intent(Settings.this, Connection.class);
+//            startActivity(intent);
+//            finish();
+//        });
+//        ImageButton backButton = findViewById(R.id.backButton);
+//        backButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(Settings.this, Contacts.class);
+//            startActivity(intent);
+//            finish();
+//        });
+//    }
+//
+//    private void updateLanguage() {
+//        LocaleHelper.setLocale(this, LocaleHelper.getSelectedLanguage(this));
+//        recreate();
+//    }
+//
+//    private int getThemeId() {
+//        SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+//        themeIndex = sharedPreferences.getInt("themeIndex", 0);
+//        switch (themeIndex) {
+//            case 1:
+//                return R.style.Theme_AP2_Ex4_Theme2;
+//            case 2:
+//                return R.style.Theme_AP2_Ex4_Theme3;
+//            default:
+//                return R.style.Theme_AP2_Ex4_Theme1;
+//        }
+//    }
+//
+//}
 package com.example.ap2_ex4;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Switch;
-import androidx.appcompat.app.AlertDialog;
+import android.content.res.Configuration;
 import android.annotation.SuppressLint;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.ap2_ex4.contacts.Contacts;
 
 public class Settings extends AppCompatActivity {
-
-    private int themeIndex = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(getThemeId());
         LocaleHelper.setLocale(this, LocaleHelper.getSelectedLanguage(this));
         setContentView(R.layout.settings);
 
@@ -37,27 +127,18 @@ public class Settings extends AppCompatActivity {
 
         @SuppressLint("UseSwitchCompatOrMaterialCode")
         Switch switchDayNightMode = findViewById(R.id.switchDayNightMode);
-        switchDayNightMode.setText(R.string.dayMode);
+        switchDayNightMode.setChecked((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES);
+        switchDayNightMode.setText(switchDayNightMode.isChecked() ? R.string.nightMode : R.string.dayMode);
         switchDayNightMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 switchDayNightMode.setText(R.string.nightMode);
             } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 switchDayNightMode.setText(R.string.dayMode);
             }
+            recreate();
         });
-
-        Button buttonChangeTheme = findViewById(R.id.buttonThemeColor);
-        buttonChangeTheme.setOnClickListener(v -> {
-            new AlertDialog.Builder(this)
-                    .setTitle("Choose Theme")
-                    .setItems(new String[]{"Theme 1", "Theme 2", "Theme 3"}, (dialog, which) -> {
-                        SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
-                        sharedPreferences.edit().putInt("themeIndex", which).apply();
-                        recreate();
-                    })
-                    .create().show();
-        });
-
         Button buttonLogout = findViewById(R.id.buttonLogout);
         buttonLogout.setOnClickListener(v -> {
             Intent intent = new Intent(Settings.this, Connection.class);
@@ -76,18 +157,4 @@ public class Settings extends AppCompatActivity {
         LocaleHelper.setLocale(this, LocaleHelper.getSelectedLanguage(this));
         recreate();
     }
-
-    private int getThemeId() {
-        SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
-        themeIndex = sharedPreferences.getInt("themeIndex", 0);
-        switch (themeIndex) {
-            case 1:
-                return R.style.Theme_AP2_Ex4_Theme2;
-            case 2:
-                return R.style.Theme_AP2_Ex4_Theme3;
-            default:
-                return R.style.Theme_AP2_Ex4_Theme1;
-        }
-    }
-
 }
